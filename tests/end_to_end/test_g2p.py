18e2a862458a4fb53885e7523911bc9bd05b3cdf
Brian Walsh <brian@bwalsh.com>
2016-06-08 09:36:41 -0700

    genotypes search by ExternalIdentifier
    	modified:   ga4gh/backend.py
    	modified:   ga4gh/datamodel/genotype_phenotype.py
    	modified:   tests/end_to_end/test_g2p.py

--- tests/end_to_end/test_g2p.py
+++ tests/end_to_end/test_g2p.py
@@ -66,22 +66,18 @@ class TestG2P(unittest.TestCase):
         # there should be at least one entry
         self.assertGreater(len(response.phenotypeAssociationSets), 0)
 
-    @unittest.skip
-    def testFeaturesSearch(self):
-        request = protocol.SearchFeaturesRequest()
+    def testGenotypesSearchByExternalIdentifier(self):
+        request = protocol.SearchGenotypesRequest()
         request.phenotypeAssociationSetId = self.getPhenotypeAssociationSetId()
         # setup the external identifiers query
-        idquery = protocol.ExternalIdentifierQuery()
         extid = protocol.ExternalIdentifier()
-        extid.identifier = "rs6920220"
+        # http://www.ncbi.nlm.nih.gov/SNP/121908585
+        extid.identifier = "121908585"
         extid.version = "*"
         extid.database = "dbSNP"
-        idquery.ids = [extid]
-        # setup the term query
-        termquery = protocol.TermQuery()
-        termquery.term = idquery
-        request.termQueries = [termquery]
-        response = self.sendPostRequest('features/search', request)
+        request.externalIdentifiers = [extid]
+        response = self.sendPostRequest('/genotypes/search', request)
+        print(response.data)
         self.assertEqual(200, response.status_code)
 
     def testPhenotypesSearchById(self):
@@ -92,7 +88,6 @@ class TestG2P(unittest.TestCase):
         postUrl = '/phenotypes/search'
         response = self.sendPostRequest(postUrl, request)
         self.assertEqual(200, response.status_code)
-        print(response.data)
         response = protocol.SearchPhenotypesResponse() \
                            .fromJsonString(response.data)
         self.assertEqual(request.id, response.phenotypes[0].id)
