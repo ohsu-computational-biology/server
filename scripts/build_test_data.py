660f632a27000813bd92f516fce50be8234b4435
Brian Walsh <brian@bwalsh.com>
2016-07-07 09:19:30 -0700

    added className to FeatureSet repo

--- scripts/build_test_data.py
+++ scripts/build_test_data.py
@@ -53,7 +53,7 @@ def main(args):
     for j, dataFile in enumerate(glob.glob(pattern)):
         run(
             "add-featureset", repoFile, datasetName, useRelativePath,
-            dataFile, "-R NCBI37", "-O", sequenceOntologyName)
+            dataFile, "-R NCBI37", "-O", sequenceOntologyName ,"-C sequenceAnnotations.Gff3DbFeatureSet" )
 
     pattern = os.path.join(prefix, "datasets/dataset1/phenotypes", "*")
     for dataFile in glob.glob(pattern):
@@ -62,7 +62,7 @@ def main(args):
         run("add-g2p", repoFile, dataFile, datasetName, "-n {}".format(name))
         run(
             "add-featureset", repoFile, datasetName, useRelativePath,
-            dataFile, "-R NCBI37",  "-O", sequenceOntologyName)
+            dataFile, "-R NCBI37",  "-O", sequenceOntologyName, ,"-C ga4gh.datamodel.genotype_phenotype_featureset.PhenotypeAssociationFeatureSet")
 
 
 def parseArgs():
